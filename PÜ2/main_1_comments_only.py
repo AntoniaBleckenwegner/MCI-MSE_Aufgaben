f1c3b4e6b05576c9e7f911d216f4f4e67d2ad970
Jannik245 <101809004+Jannik245@users.noreply.github.com>
2022-03-26 12:22:22 +0100

    Update main_1_comments_only.py

--- PÜ2/main_1_comments_only.py
+++ PÜ2/main_1_comments_only.py
@@ -1,24 +1,26 @@
 # UC 2.0
 
 #%% UC 2.1 Einlesen der Daten
-# File mittels Read Befehl einlesen
-
+# File mittels Read Befehl und entsprechender Funktion einlesen
 
 #%% UC 2.2 Vorverarbeiten der Daten
-# Die Daten ins richtige Format bringen und split-Befehl anwenden
+# Die Daten ins richtige Format bringen und dabei split-Befehl anwenden
 
 #%% UC 2.3 Analysieren der Daten auf Abbruchkriterien
-#falls Moving-Average einen bestimmten Grenzwert übersteigt soll das Programm abgebrochen werden
-#Umsetzen mit einer if-Abfrage (if movingavg > bestimmterWert).....
-#eventuell Mitteilung printen
+# falls der Moving-Average einen bestimmten Grenzwert übersteigt, soll das Programm abgebrochen werden
+# Umsetzen mit einer if-Abfrage (if movingavg > bestimmterWert)...
+# eventuell Mitteilung printen
 
 #%% UC 2.4 Erstellen einer Zusammenfassung
-#Daten zusammenfassen und in z.B.: einem Array abspeichern
+# Daten zusammenfassen und in z.B.: einem Array abspeichern
 
 #%% UC 2.5 Visualisierung der Daten
-# die ausgewerteten/zusammengefassten Daten, also den Array, in einem Plot anzeigen
+# die ausgewerteten/zusammengefassten Daten, also den Array, in einem Plot anzeigen lassen
+# eventuell Umsetzung mittels DASH
 
 #%% UC 2.6 Manuelle Eingabe eines Abbruch-Kriteriums
+# evt individuelle maximale Herzfrequenz festlegen, bei der der Test abgebrochen wird
+
 
 #%% UC 2.7 Speichern der Daten
 # Daten in einer extra Datei speichern mit Befehl datei.write()..
