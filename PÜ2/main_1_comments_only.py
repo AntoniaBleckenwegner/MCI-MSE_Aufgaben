86bec056a913d830b03eea224db8a3e1c1655025
Antonia Bleckenwegner <antonia.blecki25@gmail.com>
2022-03-26 09:15:32 +0100

    Kommentare zur Erklärung

--- PÜ2/main_1_comments_only.py
+++ PÜ2/main_1_comments_only.py
@@ -1,4 +1,29 @@
 # UC 2.0
 
 #%% UC 2.1 Einlesen der Daten
+# File mittels Read Befehl einlesen
+
+
+#%% UC 2.2 Vorverarbeiten der Daten
+# Die Daten ins richtige Format bringen und split-Befehl anwenden
+
+#%% UC 2.3 Analysieren der Daten auf Abbruchkriterien
+#falls Moving-Average einen bestimmten Grenzwert übersteigt soll das Programm abgebrochen werden
+#Umsetzen mit einer if-Abfrage (if movingavg > bestimmterWert).....
+#eventuell Mitteilung printen
+
+#%% UC 2.4 Erstellen einer Zusammenfassung
+#Daten zusammenfassen und in z.B.: einem Array abspeichern
+
+#%% UC 2.5 Visualisierung der Daten
+# die ausgewerteten/zusammengefassten Daten, also den Array, in einem Plot anzeigen
+
+#%% UC 2.6 Manuelle Eingabe eines Abbruch-Kriteriums
+
+#%% UC 2.7 Speichern der Daten
+# Daten in einer extra Datei speichern mit Befehl datei.write()..
+
+
+
+
 
